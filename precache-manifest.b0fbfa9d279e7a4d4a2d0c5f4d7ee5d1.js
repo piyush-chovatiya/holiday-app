b019f4ec86291c9f699ddf26fa220d529779ff97
Piyush Chovatiya <piyush.chovatiya@abbott.com>
2021-07-25 14:33:12 +0530

    Updates

---
+++ precache-manifest.b0fbfa9d279e7a4d4a2d0c5f4d7ee5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd10738c8fe5ddacae00a1d5fb6b77e",
+    "url": "/holiday-app/index.html"
+  },
+  {
+    "revision": "a0ea28bcb1d9d73bd55e",
+    "url": "/holiday-app/static/css/main.b17a1c58.chunk.css"
+  },
+  {
+    "revision": "906e44e8636dc47227bb",
+    "url": "/holiday-app/static/js/2.dfd40556.chunk.js"
+  },
+  {
+    "revision": "a0ea28bcb1d9d73bd55e",
+    "url": "/holiday-app/static/js/main.ff898cae.chunk.js"
+  },
+  {
+    "revision": "5a4a0cb495690c3aca80",
+    "url": "/holiday-app/static/js/runtime~main.e36f91ea.js"
+  },
+  {
+    "revision": "968d3a4a098fc93aae92547a77a0c37a",
+    "url": "/holiday-app/static/media/loader.968d3a4a.gif"
+  }
+]);
